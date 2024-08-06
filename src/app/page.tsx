f7207c78a6850b6e88e0062370374c8a54043a5a
patiphan24 <patiphun2016@gmail.com>
2024-08-06 17:48:46 +0700

    end

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 
+import UserCard from "@/components/UserCard";
+import { cleanUser } from "@/libs/cleanUser";
+import { UserCardProps } from "@/libs/types";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function RandomUserPage() {
   // annotate type for users state variable
-  const [users, setUsers] = useState(null);
-
+  const [users, setUsers] = useState<UserCardProps[]>([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [genAmount, setGenAmount] = useState(1);
+  const [genAmount, setGenAmount] = useState<number>(typeof window !== "undefined" ? Number(localStorage.getItem("genAmount")) : 1)
 
   const generateBtnOnClick = async () => {
     setIsLoading(true);
@@ -19,9 +21,17 @@ export default function RandomUserPage() {
     const users = resp.data.results;
 
     //Your code here
+    const newusers = users.map(cleanUser);
+    setUsers(newusers);
+
     //Process result from api response with map function. Tips use function from /src/libs/cleanUser
     //Then update state with function : setUsers(...)
   };
+
+  useEffect(() => {
+    const jsonAmount = JSON.stringify(genAmount);
+    localStorage.setItem("genAmount", jsonAmount);
+  }, [genAmount]);
 
   return (
     <div style={{ maxWidth: "700px" }} className="mx-auto">
@@ -32,7 +42,7 @@ export default function RandomUserPage() {
           className="form-control text-center"
           style={{ maxWidth: "100px" }}
           type="number"
-          onChange={(e) => setGenAmount(e.target.value)}
+          onChange={(e) => setGenAmount(e.target.valueAsNumber)}
           value={genAmount}
         />
         <button className="btn btn-dark" onClick={generateBtnOnClick}>
@@ -42,7 +52,15 @@ export default function RandomUserPage() {
       {isLoading && (
         <p className="display-6 text-center fst-italic my-4">Loading ...</p>
       )}
-      {users && !isLoading && users.map(/*code map rendering UserCard here */)}
+      {users && !isLoading && users.map((UC) => (
+        <UserCard
+          name={UC.name}
+          imgUrl={UC.imgUrl}
+          address={UC.address}
+          email={UC.email}
+          key={UC.email}
+        />
+      ))}
     </div>
   );
 }
